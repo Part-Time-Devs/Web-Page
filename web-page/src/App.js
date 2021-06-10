1598a0064a7635fb8035f1ee1b83fe28b9dbb08e
Alex Edwards <alexaustin9816@gmail.com>
2021-06-10 01:06:25 -0400

    react app and nav component

---
+++ web-page/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from "./Nav.js"
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ web-page/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav">
+          <img className="navImg" src="../Content/"
+          <button> <a href="#"> Home </a> </button>
+          <button> <a href="#"> About </a> </button>
+          <button> <a href="#"> Curriculum </a> </button>
+          <button> <a href="#"> Contact </a> </button>
+        </div>
+    )
+}
+
+export default Nav
