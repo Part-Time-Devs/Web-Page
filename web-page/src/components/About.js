1b1ab29153f03cfa25a5ab205b0bab4b183f8d18
Alex Edwards <alexaustin9816@gmail.com>
2021-06-10 19:48:36 -0400

    finished the contact page

--- web-page/src/components/About.js
+++ web-page/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
     return (
         <div className="about">
-          <h1> I am the about page </h1>
+          
         </div>
     )
 }
--- web-page/src/components/Contact.js
+++ web-page/src/components/Contact.js
@@ -6,21 +6,29 @@ import PTDevImg from "../content/Part Time Devs Logo Final Files.png"
 function Contact() {
     return (
         <div className="contact">
+
           <div className="discord">
             <iframe src="https://discord.com/widget?id=845428075475107890&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
             </iframe>
           </div>
-          <div className="github">
+
+          <div className="githubMain">
             <a href="https://github.com/Part-Time-Devs"> <img className="contactImg" src={PTDevImg} alt="PTDevsLogo" /> </a>
-            <a href="https://github.com/BrandonWorobi">  <img src="" alt="Brandon W" /> </a>
-            <a href="https://github.com/Rory-Ray"> <img src="" alt="Rory" /> </a>
-            <a href="https://github.com/bverfurth"> <img src="" alt="Brandon V" /> </a>
-            <a href="https://github.com/irasanchez"> <img src="" alt="Ira" /> </a>
-            <a href="https://github.com/SelfCheckout94"> <img src="" alt="Chris" /> </a>
-            <a href="https://github.com/SethBlakley"> <img src="" alt="Seth" /> </a>
-            <a href="https://github.com/TracyB85"> <img src="" alt="Tracy" /> </a>
-            <a href="https://github.com/JamariaSims"> <img src="" alt="Jamaria" /> </a>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere fermentum enim, id scelerisque leo finibus a. <br/> Curabitur pulvinar cursus finibus. Duis varius tellus ut bibendum aliquam. Curabitur nec hendrerit felis, vel hendrerit ligula. </p>
+            <div className="github">
+               <a href="https://github.com/BrandonWorobi">  <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Brandon W" /> </a>
+               <a href="https://github.com/Rory-Ray"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Rory" /> </a>
+               <a href="https://github.com/bverfurth"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Brandon V" /> </a>
+               <a href="https://github.com/irasanchez"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Ira" /> </a>
+               <a href="https://github.com/SelfCheckout94"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Chris" /> </a>
+               <a href="https://github.com/SethBlakley"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Seth" /> </a>
+               <a href="https://github.com/TracyB85"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Tracy" /> </a>
+               <a href="https://github.com/JamariaSims"> <img className="octocat" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Jamaria" /> </a>
+            </div>
           </div>
+
+
+
         </div>
     )
 }
